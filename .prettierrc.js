febd1686b7d30597ee59651ef683323679a6a3d0
ClementCatel <clement.catel4@gmail.com>
2021-06-06 03:54:34 +0200

    reset project

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: false,
+  trailingComma: "all",
+};
---
+++ src/components/global/CountDown.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ timerCount }}
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["answered"],
+
+  data() {
+    return {
+      timerCount: 15,
+    };
+  },
+
+  watch: {
+    timerCount: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 1000);
+        } else {
+          this.$emit("answered");
+        }
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
