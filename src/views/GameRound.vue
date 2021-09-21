3d3289573684a052a5376a52e8b5fdd891466ce1
jeremlm <forz_darkz@hotmail.fr>
2021-09-22 00:36:48 +0200

    feat/add round number

---
+++ src/views/GameRound.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="10" class="pl-0">
+        <v-card-text class="px-10 white--text mb-5">
+          <h1>1/10</h1>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
